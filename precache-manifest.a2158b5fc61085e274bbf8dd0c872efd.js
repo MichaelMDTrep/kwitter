8a15d48698c69b2500b9d3425737f031f8705e03
Michael Trepanier <michaelmdtrep@gmail.com>
2021-02-04 17:25:48 -0500

    Updates

---
+++ precache-manifest.a2158b5fc61085e274bbf8dd0c872efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51f101300c4bf9d723e1149c5fa88b7",
+    "url": "/kwitter/index.html"
+  },
+  {
+    "revision": "e99b7d26e0447d7bd647",
+    "url": "/kwitter/static/css/main.01184dcb.chunk.css"
+  },
+  {
+    "revision": "f5a9dbfaa7dfcfa9d857",
+    "url": "/kwitter/static/js/2.3ceefdd7.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/kwitter/static/js/2.3ceefdd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99b7d26e0447d7bd647",
+    "url": "/kwitter/static/js/main.e29b00f2.chunk.js"
+  },
+  {
+    "revision": "01880ec8fbec2b8f5a9f",
+    "url": "/kwitter/static/js/runtime-main.6f88c9a4.js"
+  }
+]);
